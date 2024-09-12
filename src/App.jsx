c199b86e013c9cedd5eeae9e9a131c49b18f9d38
symonmuchemi <simonkenya846@gmail.com>
2024-09-12 16:28:18 +0300

    initial state
    at this point the with of the div is perfectly okay

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="side-bar">
+        <p id="title">BookNest</p>
+        <div className="links">
+          <p>Books</p>
+          <p>Transacations</p>
+          <p>Members</p>
+        </div>
+      </div>
+      <div className="main">
+        <h1>BookNest</h1>
+        <h2>Your handy library management tool.</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
