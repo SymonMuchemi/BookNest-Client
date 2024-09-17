158a1b33ba7ae4ab3ae6df78ca42f6bc88190d33
symonmuchemi <simonkenya846@gmail.com>
2024-09-17 20:50:27 +0300

    refactor: Move utils.js to api.js and update API endpoints
    This commit moves the file `utils.js` to `api.js` in the `util` directory. It also updates the API endpoints for fetching books, fetching books by author, fetching books by title, and deleting a book. The API base URL is now retrieved from the environment variable `VITE_API_BASE_URL`.
    
    #refactor #api #endpoints

---
+++ src/utils/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+    baseURL: API_BASE_URL
+})
+
+export const fetchBooks = (params = '') => api.get(`/books/get_books${params}`);
+export const fetchBooksByAuthor = (author) => api.get(`/books/get_by_author/${author}`);
+export const fetchBooksByTitle = (title) => api.get(`/books/get_by_title/${title}`);
+export const deleteBook = (id) => api.delete(`/books/delete/${id}`)
+
+export default api;
