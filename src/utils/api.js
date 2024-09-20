1f5e9fe71c76d6214052c69cc26e932e9ac52f6d
symonmuchemi <simonkenya846@gmail.com>
2024-09-20 20:53:57 +0300

    update: corrects the transaction routes with proper header

--- src/utils/api.js
+++ src/utils/api.js
@@ -16,6 +16,7 @@ export const fetchBooksBySearch = (searchType, searchTerm, page = 1, perPage = 1
 export const deleteBook = (id) => api.delete(`/books/delete/${id}`);
 export const createBook = (bookData) => api.post('/books/create', bookData);
 export const updateBook = (book) => api.put(`/books/update/${book.id}`, book);
+export const getBookById = (id) => api.get(`/get_by_id/${id}`)
 
 export const fetchMembers = (page = 1, perPage = 10) => {
     return api.get(`/members/get_members`, {
@@ -25,5 +26,23 @@ export const fetchMembers = (page = 1, perPage = 10) => {
 export const deleteMember = (id) => api.delete(`/members/delete/${id}`);
 export const createMember = (memberData) => api.post('/members/create', memberData);
 export const updateMember = (member) => api.put(`/members/update/${member.id}`, member);
+export const getMemberById = (id) => api.get(`/members/get_by_id/${id}`)
+
+export const fetchTransactions = (page = 1, perPage = 10) => {
+    return api.get(`/transactions/get_transactions`, {
+        params: { page, per_page: perPage }
+    });
+};
+export const retrieveBook = (data) => api.post('/transactions/retrieve_book', data, {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export const issueBook = (data) => api.post('/transactions/issue_book', JSON.stringify(data), {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
 
 export default api;
