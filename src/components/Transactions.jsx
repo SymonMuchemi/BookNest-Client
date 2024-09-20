3e5d27f6abece7efa37a73041227c58ad82fc5ac
symonmuchemi <simonkenya846@gmail.com>
2024-09-20 20:53:00 +0300

    feat: creates the Transactions components

---
+++ src/components/Transactions.jsx
@@ -0,0 +1,102 @@
+import { useState, useCallback, useEffect } from "react";
+import { fetchTransactions } from "../utils/api";
+import RetrieveBook from "./RetrieveBook";
+import IssueBook from "./IssueBook";
+
+function Transactions() {
+  const [transactions, setTransactions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  let perPage = 10;
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    let response;
+    try {
+      response = await fetchTransactions(currentPage, perPage);
+      setTransactions(response.data.transactions);
+      setTotalPages(response.data.total_pages);
+    } catch (err) {
+      setError("Error loading Transactions");
+      console.error(err);
+    }
+    setIsLoading(false);
+  }, [currentPage, perPage]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <div className="records">
+      <h1>Transactions</h1>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {transactions.length > 0 ? (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Member Name</th>
+                <th>Book Title</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transactions.map((transaction) => {
+                const dateObj = new Date(transaction.date);
+                const date = dateObj.toLocaleDateString();
+
+                return (
+                  <tr key={transaction.id}>
+                    <td>{date}</td>
+                    <td>
+                      {transaction.member_name} [{transaction.member_id}]
+                    </td>
+                    <td>
+                      {transaction.book_title} [{transaction.book_id}]
+                    </td>
+                    <td>{transaction.type}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="pagination">
+            <button
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Previous
+            </button>
+            <span>{`Page ${currentPage} of ${totalPages}`}</span>
+            <button
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
+          <div className="record-actions">
+            <RetrieveBook />
+            <IssueBook />
+          </div>
+        </>
+      ) : (
+        <p>No Transactions found.</p>
+      )}
+    </div>
+  );
+}
+
+export default Transactions;
