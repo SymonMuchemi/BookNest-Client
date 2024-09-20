633f0e055c3e4e1e682b4478c825c8f92303ab66
symonmuchemi <simonkenya846@gmail.com>
2024-09-20 15:59:25 +0300

    update: uses routes to render components based on links

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,59 @@
 import "./App.css";
 import { Book, RefreshCw, Users } from "lucide-react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 import Books from "./components/Books";
+import Members from "./components/Member";
 
 function App() {
   return (
-    <div className="container">
-      <div className="side-bar">
-        <div id="title">
-          <img src="library-100.png" alt="shelf icon" />
-          <p>BookNest</p>
+    <Router>
+      <div className="container">
+        <div className="side-bar">
+          <div id="title">
+            <img src="library-100.png" alt="shelf icon" />
+            <p>BookNest</p>
+          </div>
+          <nav className="links">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              <Book size={20} className="icons" />
+              Books
+            </NavLink>
+            <NavLink
+              to="/transactions"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              <RefreshCw size={20} className="icons" />
+              Transactions
+            </NavLink>
+            <NavLink
+              to="/member"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              <Users size={20} className="icons" />
+              Members
+            </NavLink>
+          </nav>
         </div>
-        <nav className="links">
-          <a href="#" className="active">
-            <Book size={20} className="icons" />
-            Books
-          </a>
-          <a href="#">
-            <RefreshCw size={20} className="icons" />
-            Transactions
-          </a>
-          <a href="#">
-            <Users size={20} className="icons" />
-            Members
-          </a>
-        </nav>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="/member" element={<Members />} />
+            <Route
+              path="/transactions"
+              element={<p>Transactions page (to be implemented)</p>}
+            />
+          </Routes>
+        </div>
       </div>
-      <div className="main">
-        <Books />
-      </div>
-    </div>
+    </Router>
   );
 }
 
