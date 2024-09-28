950413b912dafdf9c731c9ea3dad3f0c3de8022c
symonmuchemi <simonkenya846@gmail.com>
2024-09-28 23:34:00 +0300

    feat: creates transaction loading skeleton

---
+++ src/components/transactions/TransactionsSkeleton.jsx
@@ -0,0 +1,36 @@
+import '../skeleton.css';
+
+const TransactionsSkeleton = () => (
+  <div className="skeleton-wrapper">
+    <div className="skeleton-header"></div>
+    <div className="skeleton-table">
+      <div className="skeleton-row header">
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+        <div className="skeleton-cell"></div>
+      </div>
+      {[...Array(5)].map((_, index) => (
+        <div key={index} className="skeleton-row">
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+          <div className="skeleton-cell"></div>
+        </div>
+      ))}
+    </div>
+    <div className="skeleton-pagination">
+      <div className="skeleton-button"></div>
+      <div className="skeleton-text"></div>
+      <div className="skeleton-button"></div>
+    </div>
+  </div>
+);
+
+export default TransactionsSkeleton;
